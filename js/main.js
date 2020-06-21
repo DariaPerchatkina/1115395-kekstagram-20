f969c173b9d596573b264f293b360cf1a4e01e2a
Дарья <perchatkinadaria@gmail.com>
2020-06-22 01:20:10 +0400

    начало

--- js/main.js
+++ js/main.js
@@ -13,6 +13,11 @@ var NAMES = ['Аня', 'Саша', 'Маша', 'Иван', 'Никита', 'Юл
 var COUNT = 25; // счетчик числа фотографий
 var likesMin = 15; // мин число лайков у фото
 var likesMax = 250; // макс число лайков у фото
+var openUploadFile = document.querySelector('.img-upload__input'); // находит в разметке по id скрытый инпут
+var uploadCancel = document.querySelector('#upload-cancel'); // находит в разметке по id кнопку отмены
+var uploadForm = document.querySelector('.img-upload__overlay'); // находит в разметке по id форму
+var ESC_KEY = 'Escape';
+var ENTER_KEY = 'Enter';
 
 // находит случайное целое число в указанном диапазоне
 var getRandomValue = function (min, max) {
@@ -94,5 +99,28 @@ var renderPhotos = function (photoElem) {
 };
 renderPhotos(photos);
 
+
+var formOpen = function () { // описывает открытие формы
+  uploadForm.classList.remove('hidden'); // у формы в расметке удаляет класс hidden
+  document.body.classList.add('modal-open'); // добавляет body класс modal-open
+};
+
+var formClose = function () { // функция закрытия формы
+  uploadForm.classList.add('hidden'); // добавляет класс hidden
+  document.body.classList.remove('modal-open'); // удаляет класс открытия модального окна
+};
+
+var onPopupEscPress = function (evt) { // управление модалкой при помощи клавиатуры
+  if (evt.key === ESC_KEY) { // если событие с клавиатуры строго равно значению эскейп на клавиатуре, то вызовется функция закрытия попапа
+
+    formClose();
+  }
+};
+
 var uploadFile = document.querySelector('#upload-file');
+
+uploadFile.addEventListener('change', function () {
+  document.removeEventListener('keydown', onPopupEscPress);
+});
+
 
