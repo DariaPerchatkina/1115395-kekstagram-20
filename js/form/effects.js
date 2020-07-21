e6f48010c2dd96ef804790f8eef7ad558373e3bf
Дарья <perchatkinadaria@gmail.com>
2020-07-22 00:52:16 +0400

    фильтр наложение

--- js/form/effects.js
+++ js/form/effects.js
@@ -2,18 +2,65 @@
 
 (function () {
   var effect = {};
-  var effectList = document.querySelector('.effects__list');
-  var imgUploadPreview = document.querySelector('.img-upload__preview');
+  var imgUploadEffectLevel = document.querySelector('.img-upload__effect-level');
+  var imgUploadPreview = document.querySelector('.img-upload__preview img');
+  var effectLevelValue = document.querySelector('.effect-level__value');
+  var effectsList = document.querySelector('.effects__list');
+  var effectLevelPin = document.querySelector('.effect-level__pin');
+  var effectLevelDepth = document.querySelector('.effect-level__depth');
+  var effectLevelLine = document.querySelector('.effect-level__line');
 
-  var onEffectListChange = function (evt) {
-    // если происходит событие и оно происходит точно на инпуте с  типом radio(evt.target.matches=true), то сбрось класс и добавь тот класс, который соответстует значению valut на текущем input
-    if (evt.target && evt.target.matches('input[type="radio"]')) {
-      imgUploadPreview.className = 'img-upload__preview effects__preview--' + evt.target.value;
+  var getFilterValue = function (filterName, percent) {
+    if (filterName === 'none') {
+      imgUploadPreview.style.filter = '';
+    }
+    if (filterName === 'chrome') {
+      imgUploadPreview.style.filter = 'grayscale(' + percent / 100 + ')';
+    }
+    if (filterName === 'sepia') {
+      imgUploadPreview.style.filter = 'sepia(' + percent / 100 + ')';
+    }
+    if (filterName === 'marvin') {
+      imgUploadPreview.style.filter = 'invert(' + percent + '%)';
+    }
+    if (filterName === 'phobos') {
+      imgUploadPreview.style.filter = 'blur(' + (percent * 3 / 100) + 'px)';
+    }
+    if (filterName === 'heat') {
+      imgUploadPreview.style.filter = 'brightness(' + percent * 3 / 100 + ')';
     }
   };
 
-  // смена фильтра
-  effectList.addEventListener('change', onEffectListChange);
+  imgUploadEffectLevel.classList.add('hidden');
 
+  var filterChange = function (evt) {
+    effectLevelValue.value = 100;
+    effectLevelPin.style.left = 100 + '%';
+    effectLevelDepth.style.width = 100 + '%';
+    imgUploadPreview.classList = '';
+    imgUploadPreview.style.filter = '';
+    imgUploadPreview.classList.add('effects__preview--' + evt.target.value);
+    if (evt.target.value !== 'none') {
+      imgUploadEffectLevel.classList.remove('hidden');
+    } else {
+      imgUploadEffectLevel.classList.add('hidden');
+    }
+  };
+  effectsList.addEventListener('change', filterChange);
+
+  var getLevelPin = function () {
+    var positionX = effectLevelPin.offsetLeft;
+    var lineWidth = effectLevelLine.offsetWidth;
+    var percent = Math.round(100 * positionX / lineWidth);
+    effectLevelValue.value = percent;
+    return percent;
+  };
+
+  var changeFilterValue = function () {
+    var current = document.querySelector('.effects__radio:checked');
+    getFilterValue(current.value, getLevelPin());
+  };
+
+  effect.changeFilterValue = changeFilterValue;
   window.effect = effect;
 })();
--- js/form/slider.js
+++ js/form/slider.js
@@ -1,66 +1,9 @@
 'use strict';
 (function () {
   var slider = {};
+  var effect = window.effect;
   var effectLevelPin = document.querySelector('.effect-level__pin');
-
-  var effectsList = document.querySelector('.effects__list');
-  var imgUploadPreview = document.querySelector('.img-upload__preview img');
-
-  var effectLevelLine = document.querySelector('.effect-level__line');
-  var effectLevelValue = document.querySelector('.effect-level__value');
   var effectLevelDepth = document.querySelector('.effect-level__depth');
-  var imgUploadEffectLevel = document.querySelector('.img-upload__effect-level');
-
-  var getFilterValue = function (filterName, percent) {
-    if (filterName === 'none') {
-      imgUploadPreview.style.filter = '';
-    }
-    if (filterName === 'chrome') {
-      imgUploadPreview.style.filter = 'grayscale(' + percent / 100 + ')';
-    }
-    if (filterName === 'sepia') {
-      imgUploadPreview.style.filter = 'sepia(' + percent / 100 + ')';
-    }
-    if (filterName === 'marvin') {
-      imgUploadPreview.style.filter = 'invert(' + percent + '%)';
-    }
-    if (filterName === 'phobos') {
-      imgUploadPreview.style.filter = 'blur(' + (percent * 3 / 100) + 'px)';
-    }
-    if (filterName === 'heat') {
-      imgUploadPreview.style.filter = 'brightness(' + percent * 3 / 100 + ')';
-    }
-  };
-
-  imgUploadEffectLevel.classList.add('hidden');
-
-  var filterChange = function (evt) {
-    effectLevelValue.value = 100;
-    effectLevelPin.style.left = 100 + '%';
-    effectLevelDepth.style.width = 100 + '%';
-    imgUploadPreview.classList = '';
-    imgUploadPreview.style.filter = '';
-    imgUploadPreview.classList.add('effects__preview--' + evt.target.value);
-    if (evt.target.value !== 'none') {
-      imgUploadEffectLevel.classList.remove('hidden');
-    } else {
-      imgUploadEffectLevel.classList.add('hidden');
-    }
-  };
-  effectsList.addEventListener('change', filterChange);
-
-  var getLevelPin = function () {
-    var positionX = effectLevelPin.offsetLeft;
-    var lineWidth = effectLevelLine.offsetWidth;
-    var percent = Math.round(100 * positionX / lineWidth);
-    effectLevelValue.value = percent;
-    return percent;
-  };
-
-  var changeFilterValue = function () {
-    var current = document.querySelector('.effects__radio:checked');
-    getFilterValue(current.value, getLevelPin());
-  };
 
   // добавляем обработчик на маркер для перемещения пина
   effectLevelPin.addEventListener('mousedown', function (evt) {
@@ -117,9 +60,10 @@
       effectLevelPin.style.left = pin.max;
       effectLevelDepth.style.width = pin.max;
     }
-    changeFilterValue();
+    effect.changeFilterValue();
   }
 
-  slider.changeFilterValue = changeFilterValue;
+  slider.effectLevelDepth = effectLevelDepth;
+  slider.effectLevelPin = effectLevelPin;
   window.slider = slider;
 })();
