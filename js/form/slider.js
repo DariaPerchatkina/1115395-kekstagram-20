a768a4646361d1f23c62b6510a958577fdd89f73
Дарья <perchatkinadaria@gmail.com>
2020-07-23 01:48:48 +0400

    первые правки после ревью

--- js/form/slider.js
+++ js/form/slider.js
@@ -4,6 +4,7 @@
   var effect = window.effect;
   var effectLevelPin = document.querySelector('.effect-level__pin');
   var effectLevelDepth = document.querySelector('.effect-level__depth');
+  var effectLevelLine = document.querySelector('.effect-level__line');
 
   // добавляем обработчик на маркер для перемещения пина
   effectLevelPin.addEventListener('mousedown', function (evt) {
@@ -30,7 +31,6 @@
       effectLevelDepth.style.width = (effectLevelPin.offsetLeft - shift.x) + 'px';
 
       setLimitCoordToSlider();
-      // window.filtereffect.setFilterEffectToImage();
     }
 
     // При подгятии мыши убраем обработчики опускания и движения мыши
@@ -45,23 +45,23 @@
     document.addEventListener('mouseup', onMouseUp);
   });
 
-  var pin = {
-    min: '0px',
-    max: '455px'
-  };
 
+  var pinLine = {
+    min: '0px',
+    max: effectLevelLine.offsetWidth - effectLevelPin.offsetWidth + 'px'
+  };
   // Функция для ограничения координат
-  function setLimitCoordToSlider() {
+  var setLimitCoordToSlider = function () {
     if (effectLevelPin.offsetLeft < 0 || effectLevelDepth.offsetLeft < 0) {
-      effectLevelPin.style.left = pin.min;
-      effectLevelDepth.style.width = pin.min;
+      effectLevelPin.style.left = pinLine.min;
+      effectLevelDepth.style.width = pinLine.min;
     }
     if (effectLevelPin.offsetLeft > 455 || effectLevelDepth.offsetLeft) {
-      effectLevelPin.style.left = pin.max;
-      effectLevelDepth.style.width = pin.max;
+      effectLevelPin.style.left = pinLine.max;
+      effectLevelDepth.style.width = pinLine.max;
     }
     effect.changeFilterValue();
-  }
+  };
 
   slider.effectLevelDepth = effectLevelDepth;
   slider.effectLevelPin = effectLevelPin;
