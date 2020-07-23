cbd290c123311298ce6dbc719180faea1eb1d253
Дарья <perchatkinadaria@gmail.com>
2020-07-24 01:30:05 +0400

    проба сети, ничего не вышло

--- js/images/createPhoto.js
+++ js/images/createPhoto.js
@@ -2,91 +2,73 @@
 
 (function () {
   var createPhoto = {};
-  // var mockData = window.mockData;
-  var form = window.form;
-  // var commentsList = document.querySelector('.social__comments'); // находит по классу в разметке список с комментариями
+  var mockData = window.mockData;
+  var commentsList = document.querySelector('.social__comments'); // находит по классу в разметке список с комментариями
 
-  // var fillPhotoTemplate = function (photo) { // создаем функцию, которая отрисовывает фото
-  //   var similarPhotoTemplate = document.querySelector('#picture') // находим элемент темплейт, куда вставим фото
-  //     .content
-  //     .querySelector('.picture');
+  var fillPhotoTemplate = function (photo) { // создаем функцию, которая отрисовывает фото
+    var similarPhotoTemplate = document.querySelector('#picture') // находим элемент темплейт, куда вставим фото
+      .content
+      .querySelector('.picture');
 
-  //   var photoElement = similarPhotoTemplate.cloneNode(true); // делаем дубликат узла template
+    var photoElement = similarPhotoTemplate.cloneNode(true); // делаем дубликат узла template
 
-  //   photoElement.querySelector('.picture__img').src = photo.url;
-  //   photoElement.querySelector('.picture__comments').textContent = photo.comments.length;
-  //   photoElement.querySelector('.picture__likes').textContent = photo.likes;
-  //   photoElement.dataset.id = photo.id;
+    photoElement.querySelector('.picture__img').src = photo.url;
+    photoElement.querySelector('.picture__comments').textContent = photo.comments.length;
+    photoElement.querySelector('.picture__likes').textContent = photo.likes;
+    photoElement.dataset.id = photo.id;
 
-  //   return photoElement; // возвращаем полученный склонированный элемент с новым содержимым
-  // };
-
-  var pictures = document.querySelector('.pictures');
-  var renderPhotos = function (photoElem) {
-    var fragment = document.createDocumentFragment(); // создаем пустой объект DocumentFragment
-    for (var i = 0; i < photoElem.length; i++) { // условия работы цикла, идет переборка массива случайно созданных фото
-      fragment.appendChild(fillPhotoTemplate(photoElem[i])); // добавляет созданное фото во фрагмент
-    }
-    pictures.appendChild(fragment); // добавляет фрагмент в разметку
+    return photoElement; // возвращаем полученный склонированный элемент с новым содержимым
   };
 
-  function onSuccess(pictures) {
-    renderPhotos(pictures);
-    pictures.addEventListener('click', function onCustomPhotoClick(evt) {
-      var picture = evt.target.closest('.picture');
-      if (picture) {
-        var id = picture.dataset.id;
-        window.bigPicture.open(pictures[id]);
-        document.addEventListener('keydown', form.onEscapePress);
+  var pictures = document.querySelector('.pictures');
+
+  window.load(function (photo) {
+    var renderPhotos = function (photoElem) {
+      var fragment = document.createDocumentFragment(); // создаем пустой объект DocumentFragment
+      for (var i = 0; i < photoElem.length; i++) { // условия работы цикла, идет переборка массива случайно созданных фото
+        fragment.appendChild(fillPhotoTemplate(photoElem[i])); // добавляет созданное фото во фрагмент
       }
-    });
-  }
+      pictures.appendChild(fragment); // добавляет фрагмент в разметку
+    };
+    renderPhotos(mockData.photos);
+  }, function () {});
 
-  function onError(errorMessage) {
-    var main = document.querySelector('main');
-    var errorBlock = document.createElement('div');
-    errorBlock.classList.add('error-block');
-    errorBlock.textContent = errorMessage;
-    main.insertAdjacentElement('afterbegin', errorBlock);
-  }
-  // renderPhotos(mockData.photos);
 
-  // // описание фотографии из рандомных данных и добавление в пустой массив commentssArr
-  // var getRandomComments = function () { // создаем функцию, которая будет генерировать случайные комментарии от пользователя
-  //   var commentsArr = []; // делаем пустой массив данных
-  //   for (var i = 0; i <= mockData.getRandomNumber(1, 10); i++) { // условия работы цикла
-  //     // в процессе работы цикла создается объект
-  //     // создадим обьект и при помощи push добавим его в массив arr(в нашем случае это пустой массив commetsArr)
-  //     commentsArr.push(mockData.getCommentObj(mockData.MESSAGES, mockData.NAMES));
-  //   }
-  //   return commentsArr;
-  // };
 
-  // var fillCommentElement = function (comment) { // создаем функцию, для формирования коммента для элемента списка
-  //   var commentItem = commentsList.querySelector('.social__comment'); // находит по классу элемент списка
-  //   var commentElement = commentItem.cloneNode(true); // делаем дубликат узла template
+  // описание фотографии из рандомных данных и добавление в пустой массив commentssArr
+  var getRandomComments = function () { // создаем функцию, которая будет генерировать случайные комментарии от пользователя
+    var commentsArr = []; // делаем пустой массив данных
+    for (var i = 0; i <= mockData.getRandomNumber(1, 10); i++) { // условия работы цикла
+      // в процессе работы цикла создается объект
+      // создадим обьект и при помощи push добавим его в массив arr(в нашем случае это пустой массив commetsArr)
+      commentsArr.push(mockData.getCommentObj(mockData.MESSAGES, mockData.NAMES));
+    }
+    return commentsArr;
+  };
 
-  //   commentElement.querySelector('.social__picture').src = comment.avatar; // заполняет найденный по классу src содердимым из объекта commentObj
-  //   commentElement.querySelector('.social__picture').alt = comment.name; // -||- alt
-  //   commentElement.querySelector('.social__text').textContent = comment.message; // -||- текст комментария
+  var fillCommentElement = function (comment) { // создаем функцию, для формирования коммента для элемента списка
+    var commentItem = commentsList.querySelector('.social__comment'); // находит по классу элемент списка
+    var commentElement = commentItem.cloneNode(true); // делаем дубликат узла template
 
-  //   return commentElement; // возвращает сформированный коммент
-  // };
+    commentElement.querySelector('.social__picture').src = comment.avatar; // заполняет найденный по классу src содердимым из объекта commentObj
+    commentElement.querySelector('.social__picture').alt = comment.name; // -||- alt
+    commentElement.querySelector('.social__text').textContent = comment.message; // -||- текст комментария
 
-  // var renderComments = function (commentsArr) {
-  //   var fragment = document.createDocumentFragment(); // создаем пустой объект DocumentFragment
+    return commentElement; // возвращает сформированный коммент
+  };
 
-  //   for (var i = 0; i < commentsArr.length; i++) { // цикл
-  //     fragment.appendChild(fillCommentElement(commentsArr[i])); // добавляет созданную фото во фрагмент
-  //   }
-  //   return fragment;
-  // };
+  window.load(function)
+  var renderComments = function (commentsArr) {
+    var fragment = document.createDocumentFragment(); // создаем пустой объект DocumentFragment
 
-  // createPhoto.pictures = pictures;
-  // createPhoto.getRandomComments = getRandomComments;
-  // createPhoto.renderComments = renderComments;
+    for (var i = 0; i < commentsArr.length; i++) { // цикл
+      fragment.appendChild(fillCommentElement(commentsArr[i])); // добавляет созданную фото во фрагмент
+    }
+    return fragment;
+  };
 
-  createPhoto.onError = onError;
-  createPhoto.onSuccess = onSuccess;
+  createPhoto.pictures = pictures;
+  createPhoto.getRandomComments = getRandomComments;
+  createPhoto.renderComments = renderComments;
   window.renderPhoto = createPhoto;
 })();
--- js/upload.js
+++ js/upload.js
@@ -7,45 +7,57 @@
   var NotFound = 404;
   var InternalServerError = 500;
   var TIME_OUT = 5000;
-  window.upload = function (url, onSuccess, onError) {
-    var xhr = new XMLHttpRequest();
 
+  window.upload = function (data, onSuccess) {
+    var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      var error;
-      switch (xhr.status) {
-        case CodeOk:
-          onSuccess(xhr.response);
-          break;
-        case BadRequest:
-          error = 'Неверный запрос';
-          break;
-        case NotFound:
-          error = 'Ничего не найдено';
-          break;
-        case InternalServerError:
-          error = 'Ошибка на стороне сервера';
-          break;
-        default:
-          error = 'Статус ответа: ' + xhr.status + ' ' + xhr.statusText;
-      }
-      if (error) {
-        onError(error);
-      }
+      onSuccess(xhr.response);
     });
 
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = TIME_OUT; // 5s
-
-    xhr.open('POST', URL_UPLOAD);
+    xhr.open('POST', URL);
     xhr.send(data);
   };
+  // window.upload = function (url, onSuccess, onError) {
+  //   var xhr = new XMLHttpRequest();
+
+  //   xhr.responseType = 'json';
+
+  //   xhr.addEventListener('load', function () {
+  //     var error;
+  //     switch (xhr.status) {
+  //       case CodeOk:
+  //         onSuccess(xhr.response);
+  //         break;
+  //       case BadRequest:
+  //         error = 'Неверный запрос';
+  //         break;
+  //       case NotFound:
+  //         error = 'Ничего не найдено';
+  //         break;
+  //       case InternalServerError:
+  //         error = 'Ошибка на стороне сервера';
+  //         break;
+  //       default:
+  //         error = 'Статус ответа: ' + xhr.status + ' ' + xhr.statusText;
+  //     }
+  //     if (error) {
+  //       onError(error);
+  //     }
+  //   });
+
+  //   xhr.addEventListener('error', function () {
+  //     onError('Произошла ошибка соединения');
+  //   });
+
+  //   xhr.addEventListener('timeout', function () {
+  //     onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+  //   });
+
+  //   xhr.timeout = TIME_OUT; // 5s
+
+  //   xhr.open('POST', URL_UPLOAD);
+  //   xhr.send(data);
+  // };
 })();
